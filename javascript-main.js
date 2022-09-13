c40fd0190c6b154db9be2a5bfcaeb051a44da290
tyberry2255 <112981327+tyberry2255@users.noreply.github.com>
2022-09-13 12:46:53 -0700

    testing a sidebar mover

--- javascript-main.js
+++ javascript-main.js
@@ -1,12 +1,14 @@
-let id = null;
-const elem = document.getElementById("sidebar");   
-let pos = 316; // top of the sidebar
-id = setInterval(shiftSidebar, 20);
-function shiftSidebar() {
-  if (pos == 416) {
-    clearInterval(id);
-  } else {
-    pos++; 
-    elem.style.top = pos + "px";  
+function mainScript(){
+  let id = null;
+  const elem = document.getElementById("sidebar");   
+  let pos = 316; // top of the sidebar
+  id = setInterval(shiftSidebar, 20);
+  function shiftSidebar() {
+    if (pos == 1016) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px";  
+    }
   }
 }
