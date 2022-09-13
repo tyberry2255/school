c209036841f07591c03a8c5c0df33358687381f6
tyberry2255 <112981327+tyberry2255@users.noreply.github.com>
2022-09-13 12:36:31 -0700

    Testing to see if the sidebar goes down by 100px

--- javascript-main.js
+++ javascript-main.js
@@ -1,1 +1,12 @@
-
+let id = null;
+const elem = document.getElementById("sidebar");   
+let pos = 316; // top of the sidebar
+id = setInterval(shiftSidebar, 20);
+function shiftSidebar() {
+  if (pos == 416) {
+    clearInterval(id);
+  } else {
+    pos++; 
+    elem.style.top = pos + "px";  
+  }
+}
