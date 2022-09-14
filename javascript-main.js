5e5482e0e9e0ee38f3391eb172d5cf00b967209a
tyberry2255 <112981327+tyberry2255@users.noreply.github.com>
2022-09-14 15:41:32 -0700

    fixing the problem in a better way. I used to change the src, but now im changing the display

--- javascript-main.js
+++ javascript-main.js
@@ -13,6 +13,6 @@ function mainScript() {
   }
 }
 function button() {
-  const elem = document.getElementById("rhs-logo")
-  elem.outerHTML.src = "https://pbs.twimg.com/profile_images/378800000370800996/0b6aa5001e6d72c5294b4681cf1cd193_400x400.jpeg"
+  const elem = document.getElementById("rhs-logo");
+  elem.style.display = inline;
 }
