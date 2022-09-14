d3e9687f71e3ec7f8eb571dc1b3d75f0e708863c
tyberry2255 <112981327+tyberry2255@users.noreply.github.com>
2022-09-14 15:37:19 -0700

    testing it again, it didn't work the first time.

--- javascript-main.js
+++ javascript-main.js
@@ -14,5 +14,5 @@ function mainScript() {
 }
 function button() {
   const elem = document.getElementById("rhs-logo")
-  elem.src = "https://pbs.twimg.com/profile_images/378800000370800996/0b6aa5001e6d72c5294b4681cf1cd193_400x400.jpeg"
+  elem.outerHTML.src = "https://pbs.twimg.com/profile_images/378800000370800996/0b6aa5001e6d72c5294b4681cf1cd193_400x400.jpeg"
 }
