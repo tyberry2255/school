23733e0b4e47bf6493cd9eb7a22ca262cf0be35f
tyberry2255 <112981327+tyberry2255@users.noreply.github.com>
2022-09-14 15:55:43 -0700

    added a hover for the button as well as made the sidebar reset

--- javascript-main.js
+++ javascript-main.js
@@ -4,15 +4,19 @@ function mainScript() {
   let pos = 316; // top of the sidebar
   id = setInterval(shiftSidebar, 20);
   function shiftSidebar() {
-    if (pos == 1016) {
-      clearInterval(id);
+    if (pos >= 1016) {
+      pos = 316;
     } else {
       pos++; 
-      elem.style.top = pos + "px";  
+      elem.style.top = pos + "px";  // basically moves the sidebar down
     }
   }
 }
 function button() {
   const elem = document.getElementById("rhs-logo");
-  elem.style.display = "inline";
+  elem.style.display = "inline"; // changes the rhs logo from hidden to showing
+}
+function hover() { // when you hover over the button you get this
+  const elem = document.getElementById("rhs-logo");
+  elem.style.cursor = "pointer";
 }
