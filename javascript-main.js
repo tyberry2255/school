9f3f9d27ac87f3ef3036f58d01e83ff48a5823ab
tyberry2255 <112981327+tyberry2255@users.noreply.github.com>
2022-09-14 21:06:13 -0700

    Created the logo to turn off when you click it again

--- javascript-main.js
+++ javascript-main.js
@@ -5,7 +5,7 @@ function mainScript() {
   id = setInterval(shiftSidebar, 20);
   function shiftSidebar() {
     if (pos >= 1016) {
-      pos = 201;
+      pos = 201; // once the sidebar is all the way down, it puts it back up
     } else {
       pos = pos + 10; 
       elem.style.top = pos + "px";  // basically moves the sidebar down
@@ -14,7 +14,13 @@ function mainScript() {
 }
 function button() {
   const elem = document.getElementById("rhs-logo");
-  elem.style.display = "inline"; // changes the rhs logo from hidden to showing
+  if (time == true) {
+    elem.style.display = "none"; // changes it back
+    var time = false;
+  } else {
+    var time = true;
+    elem.style.display = "inline"; // changes the rhs logo from hidden to showing
+  } 
 }
 function hover() { // when you hover over the button you get this
   const elem = document.getElementById("rhs-logo");
